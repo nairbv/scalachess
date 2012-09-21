75a3dc8a49f2ea085f329994e358efd2a839ed44
Brian <brian@mint>
2012-09-21 11:45:12 +0800

    adding a main class to use sbt "run' for profiling purposes.

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,14 @@
+
+package net.brianvaughan.scala.ai
+
+import net.brianvaughan.scala.ai.games.chess._
+
+object Main {
+  def main(args:Array[String]) {
+    BoardState.watchAnIterativeDeepeningGame(800)
+  }
+}
+
+
+
+
